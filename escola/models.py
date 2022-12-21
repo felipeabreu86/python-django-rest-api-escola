dece257c13e537d918815a9bd509205e9ade77be
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-12-21 14:30:14 -0300

    configurando o django admin para os modelos aluno e curso

--- escola/models.py
+++ escola/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Aluno(models.Model):
     nome = models.CharField(max_length=30)
     rg = models.CharField(max_length=9)
-    cpf = models.CharField(max_length=1)
+    cpf = models.CharField(max_length=11)
     data_nascimento = models.DateField()
 
     class Meta:
