73993376829b0b1595baf14826d3be973677e381
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-12-21 13:53:11 -0300

    configurando o django rest framework

--- escola/views.py
+++ escola/views.py
@@ -3,5 +3,5 @@ from django.http import JsonResponse
 
 def alunos(request):
     if request.method == 'GET':
-        aluno = {'id': '1', 'nome': 'Felipe'}
+        aluno = {'id': 1, 'nome': 'Felipe'}
         return JsonResponse(aluno)
--- setup/settings.py
+++ setup/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'escola.apps.EscolaConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
