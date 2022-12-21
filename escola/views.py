b2e1977d8301f4c544dc594043c7f517b3f2ace4
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-12-21 15:49:04 -0300

    criando o recurso da matrícula

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer import AlunoSerializer, CursoSerializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
 
 allowed_http_methods = ['get', 'post', 'put', 'patch', 'delete']
 
@@ -16,4 +16,11 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    http_method_names = allowed_http_methods
+
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Listando todas as matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
     http_method_names = allowed_http_methods
