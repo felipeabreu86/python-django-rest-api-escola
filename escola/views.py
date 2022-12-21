222c184c2b8f40dddb0bcd54f84aaf94990e6feb
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-12-21 15:04:18 -0300

    configurando os métodos http permitidos

--- escola/views.py
+++ escola/views.py
@@ -2,14 +2,18 @@ from rest_framework import viewsets
 from escola.models import Aluno, Curso
 from escola.serializer import AlunoSerializer, CursoSerializer
 
+allowed_http_methods = ['get', 'post', 'put', 'patch', 'delete']
+
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os aluno(a)s"""
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    http_method_names = allowed_http_methods
 
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    http_method_names = allowed_http_methods
